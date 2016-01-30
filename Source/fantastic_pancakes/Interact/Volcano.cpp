e3aeeacbff83fef049a84c917e0b146b0be7c270
Douglas1701 <matthew.douglas@mytfs.ca>
2016-01-30 04:36:00 -0500

    All I know

---
+++ Source/fantastic_pancakes/Interact/Volcano.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "fantastic_pancakes.h"
+#include "Volcano.h"
+
+AVolcano::AVolcano()
+{
+
+}
+
+// Called when the game starts or when spawned
+void AVolcano::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AVolcano::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AVolcano::InteractEvent()
+{
+
+}
